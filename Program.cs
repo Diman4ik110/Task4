ea8203cf7eb65cb9bdd9fd5ec916fa203ed01d5d
Diman4ik110 <dvu.dima@mail.ru>
2021-03-05 00:15:16 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String phrase = "";
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Введите фразу: ");
+                    phrase = ClearPhrase(Console.ReadLine());
+                    Console.WriteLine(phrase);
+                    if (phrase == ReverseStr(phrase))
+                    {
+                        Console.WriteLine("Строка является палиндромом!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Строка не является палиндромом!!");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        private static string ClearPhrase(String inputPhrase)
+        {
+            // Список символов для удаления из фразы
+            string[] symbols = { "!","@","\"",",",".","?","`"," ", "<",">","^","%","$","#","/","~","(",")","_","-","+" };
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                // Удаление символа из строки
+                inputPhrase = inputPhrase.Replace(symbols[i], "");
+            }
+            // Возврат строки
+            return inputPhrase;
+        }
+        // Реверсируем строку
+        private static string ReverseStr(string InputStr)
+        {
+            string outStr = "";
+            // Цикл реверсирования строки
+            for (int i = InputStr.Length-1; i >= 0 ; i--)
+            {
+                outStr += InputStr[i];
+            }
+            // Возврат строки
+            return outStr;
+        }
+    }
+}
